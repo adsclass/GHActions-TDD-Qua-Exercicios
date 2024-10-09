c82afbab4dc9c0a1ee12cc1ce852c8ea6ed54265
ricksondeivid <deividrickson80@gmail.com>
2024-10-09 20:53:48 -0300

    avaliação

--- src/math.test.js
+++ src/math.test.js
@@ -1,4 +1,4 @@
-const { fatorial, fibonnacci, ehPrimo } = require('./math');
+const { fatorial, fibonacci, ehPrimo } = require('./math'); // Corrigido de 'fibonnacci' para 'fibonacci'
 
 describe('função fatorial', () => {
   test('valida entrada', () => {
@@ -26,24 +26,24 @@ describe('função fatorial', () => {
   });
 });
 
-describe('função fibonacci', () => {
+describe('função fibonacci', () => { // Corrigido de 'fibonnacci' para 'fibonacci'
   test('valida entrada', () => {
-    // Verifica se a função 'fibonnacci' existe
-    expect(fibonnacci).toBeDefined();
-    // Verifica se a função 'fibonnacci' possui 1 argumento
-    expect(fibonnacci.length).toBe(1);
+    // Verifica se a função 'fibonacci' existe
+    expect(fibonacci).toBeDefined();
+    // Verifica se a função 'fibonacci' possui 1 argumento
+    expect(fibonacci.length).toBe(1);
 
     // Garantir que o argumento é um número
-    expect(() => fibonnacci("Texto")).toThrow(TypeError);
-    expect(() => fibonnacci("Texto")).toThrow("O argumento deve ser um número válido");
-    expect(() => fibonnacci("10")).not.toThrow(TypeError);
-    expect(() => fibonnacci(10)).not.toThrow(TypeError);
+    expect(() => fibonacci("Texto")).toThrow(TypeError);
+    expect(() => fibonacci("Texto")).toThrow("O argumento deve ser um número válido");
+    expect(() => fibonacci("10")).not.toThrow(TypeError);
+    expect(() => fibonacci(10)).not.toThrow(TypeError);
 
-    // Garantir que não existe fibonnacci de número menor ou igual a zero
-    expect(() => fibonnacci(-10)).toThrow(Error);
-    expect(() => fibonnacci(-10)).toThrow("Não existe fibonnacci de número menor ou igual a zero");
-    expect(() => fibonnacci(0)).toThrow(Error);
-    expect(() => fibonnacci(0)).toThrow("Não existe fibonnacci de número menor ou igual a zero");
+    // Garantir que não existe fibonacci de número menor ou igual a zero
+    expect(() => fibonacci(-10)).toThrow(Error);
+    expect(() => fibonacci(-10)).toThrow("Não existe fibonnacci de número menor ou igual a zero");
+    expect(() => fibonacci(0)).toThrow(Error);
+    expect(() => fibonacci(0)).toThrow("Não existe fibonnacci de número menor ou igual a zero");
   });
   test('Retorna o número fibonacci de 8 igual a 21', () => {
     expect(fibonacci(8)).toBe(21);
@@ -76,12 +76,12 @@ describe('função ehPrimo', () => {
     expect(() => ehPrimo(-2)).toThrow("Não número primo de número menor ou igual a 1");
   });
   test('verifica que 7 e 11 são números primos', () => {
-    expect(ehPrimo( 7)).toBe(true);
+    expect(ehPrimo(7)).toBe(true);
     expect(ehPrimo(11)).toBe(true);
   });
 
   test('verifica que 6 e 10 não são números primos', () => {
-    expect(ehPrimo( 6)).toBe(false);
+    expect(ehPrimo(6)).toBe(false);
     expect(ehPrimo(10)).toBe(false);
   });
 });
