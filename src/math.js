20758cf5caa4aa1dd9db4281c58b889df76d9608
JiraiyaLoi <joaoloiola@hotmail.com>
2024-10-09 20:43:03 -0300

    Desenvolvimento das funções em src/Math.js de acordo com as especificações de teste em src/Math.test.js

--- src/math.js
+++ src/math.js
@@ -1,15 +1,60 @@
-function fatorial() {
+function fatorial(n) {
+  n = Number(n);
+  if (isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n < 0) {
+    throw new TypeError("Não existe fatorial de número negativo");
+  }
+  if (n === 0) return 1;
 
+  let resultado = 1;
+  for (let i = 1; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
 }
-function fibonnacci() {
 
+function fibonnacci(n) {
+  n = Number(n);
+  if (isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 0) {
+      throw new Error("Não existe fibonnacci de número menor ou igual a zero");
+  }
+  if (n === 1) return 1;
+  if (n === 2) return 1;
+
+  let a = 1, b = 1;
+  for (let i = 3; i <= n; i++) {
+      const temp = a + b;
+      a = b;
+      b = temp;
+  }
+  return b;
 }
-function ehPrimo() {
 
+function ehPrimo(n) {
+  n = Number(n);
+  if (isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) {
+    throw new TypeError("Não número primo de número menor ou igual a 1");
+  }
+
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+          return false;
+      }
+  }
+  return true;
 }
 
 module.exports = {
   fatorial,
   fibonnacci,
   ehPrimo
-}
+};
